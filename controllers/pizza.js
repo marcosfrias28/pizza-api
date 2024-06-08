d84ffdcc8267040efc52f2994ecd998d1d517b6d
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-06-08 14:35:59 +0200

    MODEL architecture MVC added

---
+++ controllers/pizza.js
@@ -0,0 +1,24 @@
+import { PizzaModel } from "../models/pizza.js";
+
+export class PizzaController {
+    static async getAll(req, res) {
+        const {name, ingredients} = req.query;
+        const pizza = await PizzaModel.getAllPizzas({name, ingredients});
+        res.json(pizza);
+    }
+    static async getById(req, res) {
+        const { id } = req.params;
+        const pizza = await PizzaModel.getById({id});
+        if (pizza) return res.json(pizza);
+        res.status(404).json({message: 'Pizza not found'});
+     }
+     static async create(req, res) {
+        const result = validatePizza(req.body);
+        await PizzaModel.create({input: result});
+        if (!result.success) {
+            return res.status(400).json({message: 'Invalid pizza', errors: result.error});
+        } else {
+            res.status(201).json({message: 'Pizza created successfully'});
+        }
+      }
+}
--- routes/pizza.js
+++ routes/pizza.js
@@ -1,42 +1,10 @@
 import { Router } from "express";
-import { readJSON } from "../utils/readJSON.js";
-
-
-const pizza = readJSON('../pizza.json')
+import { PizzaController } from "../controllers/pizza.js";
 
 export const pizzaRoute = Router();
 
-pizzaRoute.get('/', (req, res) => {
-    const {name, ingredients} = req.query;
-    if (name) {
-        const pizzaByName = pizza.filter(pizza => pizza.name.includes(name));
-        return res.json(pizzaByName);
-    }
-    if (ingredients) {
-        const pizzaByIngredients = pizza.filter(pizza => pizza.ingredients.some(ingredient => ingredient.toLowerCase().includes(ingredients.toLowerCase())));
-        return res.json(pizzaByIngredients);
-    }
-    res.json(pizza);
-    res.status(200);
-})
+pizzaRoute.get('/', PizzaController.getAll)
 
-pizzaRoute.get('/:id', (req, res) => {
-    const { id } = req.params;
-    const pizzaId = pizza.find(pizza => pizza.id === id);
-    if (pizzaId) return res.json(pizzaId);
-    res.status(404).json({message: 'Pizza not found'});
-})
+pizzaRoute.get('/:id', PizzaController.getById)
 
-pizzaRoute.post('/', (req, res) => {
-    const result = validatePizza(req.body);
-    if (!result.success) {
-        return res.status(400).json({message: 'Invalid pizza', errors: result.error});
-    } else {
-        res.status(201).json({message: 'Pizza created successfully'});
-    }
-    const newPizza = {
-        id: crypto.randomUUID(),
-        ...result.data
-    }
-    pizza.push(newPizza);
-})
+pizzaRoute.post('/', PizzaController.create)
